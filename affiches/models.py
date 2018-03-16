eedafdb9e2eed1d0e4db00c40213518d12b67936
Jeff Pelgrims <jeffpelgrims@gmail.com>
2018-03-16 06:06:26 +0100

    Première ébauche

--- affiches/models.py
+++ affiches/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tel = models.CharField(max_length=20, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.location
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
